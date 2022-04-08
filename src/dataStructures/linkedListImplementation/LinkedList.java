16154ed9577e9645c1296b0152b54266797565bb
ManuelGroebl <m.groebl@online.de>
2022-04-08 22:01:06 +0200

    create scaffold for LinkedList implementation class

--- src/dataStructures/linkedListImplementation/LinkedList.java
+++ src/dataStructures/linkedListImplementation/LinkedList.java
@@ -1,5 +1,52 @@
 package dataStructures.linkedListImplementation;
 
-public class LinkedList {
+import dataStructures.DataElement;
+import dataStructures.ListImplementation;
+
+public class LinkedList extends ListImplementation{
+	
+	DataElement firstElement;
+	
+	public LinkedList() {
+		
+		firstElement = null;
+		
+	}
+
+	@Override
+	public void insert(DataElement newElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DataElement remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getNElements() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DataElement[] getElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
