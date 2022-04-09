7756fc2be92b0a2b6238e9d29a20ab99dead591b
ManuelGroebl <m.groebl@online.de>
2022-04-09 23:30:22 +0200

    Add linked list testing

---
+++ src/dataStructures/LinkedListImplmentationTest.java
@@ -0,0 +1,49 @@
+package dataStructures;
+
+public class LinkedListImplmentationTest extends ImplementationTest{
+
+	@Override
+	boolean testInsert(boolean print) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean testRemove(boolean print) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean testIsEmpty(boolean print) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean testInsertOrder(boolean print) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean testRemoveOrder(boolean print) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean testGetNElements(boolean print) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	void testPrintAll() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
