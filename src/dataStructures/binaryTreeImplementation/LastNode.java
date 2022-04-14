d1e8a8f87aefd5af80d518376021f60a717d8f7a
ManuelGroebl <m.groebl@online.de>
2022-04-14 23:13:49 +0200

    Add Node and LastNode classes to binaryTree

---
+++ src/dataStructures/binaryTreeImplementation/LastNode.java
@@ -0,0 +1,43 @@
+package dataStructures.binaryTreeImplementation;
+
+import dataStructures.DataElement;
+
+public class LastNode extends TreeElement{
+
+	@Override
+	public void add(DataElement dataElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printPreOrder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printInOrder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printPostOrder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setElementsArrayInOrder(DataElement[] elements, int nElements) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
