2125a1430d240bb220253dbb17081585f1a4179e
ManuelGroebl <m.groebl@online.de>
2022-04-15 22:18:28 +0200

    Add scaffold for BinaryTree class

--- src/dataStructures/binaryTreeImplementation/BinaryTree.java
+++ src/dataStructures/binaryTreeImplementation/BinaryTree.java
@@ -1,6 +1,55 @@
 package dataStructures.binaryTreeImplementation;
 
-public class BinaryTree {
+import dataStructures.DataElement;
+import dataStructures.ListImplementation;
+
+public class BinaryTree extends ListImplementation{
+	
+	TreeElement firstElement;
+
+	//for testing
+	int arraySize;
+	
+	public BinaryTree(int arraySize) {
+		this.firstElement = new LastNode();
+		this.arraySize = arraySize;
+	}
+	
+	@Override
+	public void insert(DataElement newElement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DataElement remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getNElements() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DataElement[] getElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 }
