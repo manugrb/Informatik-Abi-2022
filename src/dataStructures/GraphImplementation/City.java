d60507ef979601f5566d8457bf54c893a7b0ed61
ManuelGroebl <m.groebl@online.de>
2022-04-18 23:11:22 +0200

    Implement DataElements and their implementations

--- src/dataStructures/GraphImplementation/City.java
+++ src/dataStructures/GraphImplementation/City.java
@@ -2,4 +2,16 @@ package dataStructures.GraphImplementation;
 
 public class City extends DataElement{
 
+	public City(String name, int attractivityScore, int nFreeHotels, int reviewData) {
+		this.name = name;
+		this.attractivityScore = attractivityScore;
+		this.nFreeHotels = nFreeHotels;
+		this.reviewData = reviewData;
+	}
+	
+	@Override
+	public String getName() {
+		return "Stadt " + name;
+	}
+
 }
--- src/dataStructures/GraphImplementation/DataElement.java
+++ src/dataStructures/GraphImplementation/DataElement.java
@@ -1,5 +1,21 @@
 package dataStructures.GraphImplementation;
 
 public abstract class DataElement {
+	
+	protected String name;
+	protected int attractivityScore;
+	protected int nFreeHotels;
+	protected int reviewData;
+	
+	public abstract String getName();
+	public int getAttractivity() {
+		return attractivityScore;
+	}
+	public int getNFreeHotels() {
+		return nFreeHotels;
+	}
+	public int getReviewData() {
+		return reviewData;
+	}
 
 }
