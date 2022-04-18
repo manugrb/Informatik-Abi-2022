fdff49049d0c6daf776d580db951420edfd121c5
ManuelGroebl <m.groebl@online.de>
2022-04-18 23:19:24 +0200

    Add first important methods in Graph

--- src/dataStructures/GraphImplementation/Graph.java
+++ src/dataStructures/GraphImplementation/Graph.java
@@ -1,5 +1,31 @@
 package dataStructures.GraphImplementation;
 
 public class Graph {
+	
+	private int[][] adjacencyMatrix;
+	Node[] nodes;
+	
+	public Graph(int maxNNodes) {
+		nodes = new Node[maxNNodes];
+		adjacencyMatrix = new int[maxNNodes][maxNNodes];
+	}
+	
+	public void addConnection(Node start, Node end, int value) {
+		
+	}
+	
+	public int getConnection(Node start, Node end) {
+		
+		return 0;
+	}
+	
+	public void printAll() {
+		
+	}
+	
+	private boolean ckeckIfInGraph(Node checkNode) {
+		
+		return false;
+	}
 
 }
