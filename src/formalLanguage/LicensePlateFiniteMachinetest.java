fed75242c0308b93f567c4c70b177710bef878a4
ManuelGroebl <m.groebl@online.de>
2022-04-22 16:42:49 +0200

    Implement correct LicensePlate test

--- src/formalLanguage/LicensePlateFiniteMachinetest.java
+++ src/formalLanguage/LicensePlateFiniteMachinetest.java
@@ -2,6 +2,30 @@ package formalLanguage;
 
 public class LicensePlateFiniteMachinetest {
 	
+	private LicensePlateFiniteMachine testObject;
 	
+	public LicensePlateFiniteMachinetest() {
+		
+		testObject = new LicensePlateFiniteMachine();
+		
+	}
+	
+	public boolean testCorrectLicensePlate() {
+		
+		boolean testPassed = true;
+		
+		testPassed = testPassed && testObject.checkString("AAA AA 0000");
+		testPassed = testPassed && testObject.checkString("ZZZ ZZ 9999");
+		testPassed = testPassed && testObject.checkString("A AA 1111");
+		testPassed = testPassed && testObject.checkString("AA AA 1111");
+		testPassed = testPassed && testObject.checkString("AAA A 1111");
+		testPassed = testPassed && testObject.checkString("AAA AA 1");
+		testPassed = testPassed && testObject.checkString("AAA AA 11");
+		testPassed = testPassed && testObject.checkString("AAA AA 111");
+		
+		return testPassed;
+		
+		
+	}
 
 }
--- src/formalLanguage/Main.java
+++ src/formalLanguage/Main.java
@@ -4,7 +4,14 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		LicensePlateFiniteMachinetest test = new LicensePlateFiniteMachinetest();
+		boolean allTestsPassed = true;
 		
+		allTestsPassed = allTestsPassed && test.testCorrectLicensePlate();
+		
+		if(allTestsPassed) {
+			System.out.println("all tests passed");
+		}
 		
 	}
 
